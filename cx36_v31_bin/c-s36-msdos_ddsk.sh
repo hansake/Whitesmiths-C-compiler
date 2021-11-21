465e62dbd0019e5609705c406c425d8691515fad
Hans-Ake <hansake@users.noreply.github.com>
2021-11-21 10:46:27 +0100

    Update c-s36-msdos_ddsk.sh

--- cx36_v31_bin/c-s36-msdos_ddsk.sh
+++ cx36_v31_bin/c-s36-msdos_ddsk.sh
@@ -2,7 +2,7 @@
 #
 # Boot MS-DOS 6.22 in a QEMU virtual machine
 # the VM contains Whitesmiths C compiler for IBM System/36
-# This start script mounts directory "dosfiles" as drive d:
+# This start script mounts directory "CX36" as drive d:
 #
 qemu-system-i386 -m 16 \
 -drive file=c-s36-msdos.img,format=raw,index=0,media=disk \
